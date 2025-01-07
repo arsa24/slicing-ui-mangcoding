1f8ee44023a21a46e0ef9520b74454e40c6bfc22
arsa24 <idunvoneinzbern@gmail.com>
2025-01-07 07:28:34 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/*.{css, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
