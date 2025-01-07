eb6ef1824eb28aa3455d1f1b9e9f3cb252054864
arsa24 <idunvoneinzbern@gmail.com>
2025-01-07 14:05:18 +0700

    major update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/*.{css, js}"
-  ],
+  content: ["./index.html", "./src/**/*.{css,js}"], // pastikan path ini sesuai dengan struktur folder
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "plus-jakarta-sans": ['"Plus Jakarta Sans"', "sans-serif"],
+        inter: ['"Inter"', "sans-serif"],
+        "halvetica-neue": ['"Halvetica neue"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
