530597e22ab423f64b1205892513e062bffc61e8
arsa24 <idunvoneinzbern@gmail.com>
2025-01-12 23:55:38 +0700

    minor update

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 import Swiper from "swiper";
 import "swiper/css";
 
-// navbar dropdown
+const w = window.innerWidth;
 
 // swiper
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
+  slidesPerView: w >= 768 ? 3 : 1,
   centeredSlides: true,
   loop: true,
   slideActiveClass: "active-slide",
@@ -23,4 +23,36 @@ acordions.forEach((item) => {
     });
     item.classList.add("active-acordion");
   });
+});
+
+const month = document.querySelector("#monthly");
+const year = document.querySelector("#yearly");
+
+function updatePricing(plan) {
+  const discount = 15;
+
+  const planCards = document.querySelectorAll(".plan-card");
+
+  planCards.forEach((card) => {
+    const dataMonth = parseFloat(card.getAttribute("data-month"));
+    const dataYear = parseFloat(card.getAttribute("data-year"));
+    const priceElement = card.querySelector(".price");
+    const timeElement = card.querySelector(".time");
+
+    if (plan === "Month") {
+    } else if (plan === "Year") {
+    }
+  });
+}
+
+month.addEventListener("click", () => {
+  updatePricing("Month");
+  month.classList.add("pricing-active");
+  year.classList.remove("pricing-active");
+});
+
+year.addEventListener("click", () => {
+  updatePricing("Year");
+  year.classList.add("pricing-active");
+  month.classList.remove("pricing-active");
 });
